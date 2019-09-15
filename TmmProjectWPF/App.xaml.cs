ee80eae7162e36c8108104f553f9eb4f1d2cb6a6
vmvpro <melnvetal@gmail.com>
2019-09-15 22:17:47 +0300

    Добавление OnStartup()

--- TmmProjectWPF/App.xaml.cs
+++ TmmProjectWPF/App.xaml.cs
@@ -13,5 +13,10 @@ namespace TmmProjectWPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
     }
 }
