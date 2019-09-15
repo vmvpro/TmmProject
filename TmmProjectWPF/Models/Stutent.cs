db8820342f23080edd85a7d6e72a4c68b5589827
vmvpro <melnvetal@gmail.com>
2019-09-16 00:03:33 +0300

    Реализация инерфейса INotifyPropertyChanged для класса Student

---
+++ TmmProjectWPF/Models/Stutent.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace TmmProjectWPF.Models
+{
+    internal class Stutent : INotifyPropertyChanged
+    {
+        private string name;
+
+        public Stutent(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                //OnPropertyChanged(nameof(name));
+                OnPropertyChanged("Name");
+
+            }
+        }
+        
+        #region INotifyPropertyChanged Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
